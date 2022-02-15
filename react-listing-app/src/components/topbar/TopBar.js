e03d86f6bd70e0a5a9c0f659fbb2a604b3c72305
SIBChoi <qorwh1224@naver.com>
2022-02-15 14:41:56 +0900

    [ADD] TobBar & css

---
+++ react-listing-app/src/components/topbar/TopBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './TopBar.module.css';
+
+const TopBar = () => {
+  return (
+    <div className={styles.topbar}>
+      <div className={styles.left}>
+        <i className={`${styles['social-icons']} fa-brands fa-facebook`}></i>
+        <i className={`${styles['social-icons']} fa-brands fa-instagram`}></i>
+        <i className={`${styles['social-icons']} fa-brands fa-linkedin`}></i>
+        <i className={`${styles['social-icons']} fa-brands fa-twitter`}></i>
+      </div>
+      <div className={styles.center}>
+        <ul className={styles.list}>
+          <li className={styles['list-item']}>HOME</li>
+          <li className={styles['list-item']}>ABOUT</li>
+          <li className={styles['list-item']}>CONTACT</li>
+          <li className={styles['list-item']}>CREATE LISTING</li>
+          <li className={styles['list-item']}>LOG OUT</li>
+        </ul>
+      </div>
+      <div className={styles.right}>
+        <img
+          className={styles['profile-img']}
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+          alt="profile"
+        />
+        <i className={`${styles['search-icon']} fa-brands fa-searchengin`}></i>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
